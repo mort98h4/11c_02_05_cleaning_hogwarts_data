54c4d58956e755f9ff37cfd91d1c59ed44af8988
Morten Gross <mg@vuffeli.dk>
2021-02-12 16:56:23 +0100

    json is fetched

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", accioJsonData);
+
+let theStudents;
+const Student = {
+    firstName: "",
+    lastName: "",
+    middleName: "",
+    nickName: "",
+    imageUrl: "",
+    house: ""
+};
+
+async function accioJsonData() {
+    console.log("Accio, JSON data!");
+    const url = "https://petlatkea.dk/2021/hogwarts/students.json";
+    const jsonData = await fetch(url);
+    theStudents = await jsonData.json();
+    console.log(theStudents);
+}
